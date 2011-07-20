31a501e81f1697ece54c2442622157c88807ec40
Mathieu Boespflug <mboes@cs.mcgill.ca>
2011-07-19 22:23:49 -0400

    Make parser combination function composition again.

--- Bi.hs
+++ Bi.hs
@@ -1,7 +1,5 @@
 module Bi where
 
-import Control.Monad
-import Data.Maybe
 import Data.List
 
 
@@ -9,73 +7,66 @@ data Bi a b = Bi { first :: a, second :: b }
 
 infixr 9 <>
 
--- (<>) :: Bi (b -> c) (c' -> b') -> Bi (a -> b) (b' -> a') -> Bi (a -> c) (c' -> a')
--- Bi f f' <> Bi g g' = Bi (f . g) (g' . f')
-
---Bi f f' <> Bi g g' = Bi (f . g) (g' . f')
-Bi f f' <> Bi g g' = Bi (\k -> f k . g (const k))
-                        (\k -> g' (const k) . f' k)
+(<>) :: Bi (b -> c) (c' -> b') -> Bi (a -> b) (b' -> a') -> Bi (a -> c) (c' -> a')
+Bi f f' <> Bi g g' = Bi (f . g) (g' . f')
 
 infixr 8 -->
-Bi f f' --> Bi g g' = Bi (\k -> f k . g k) (\k -> g' k . f' k)
+(-->) = (<>)
 
---newtype ContT r m a  = ContT { runContT  :: (String -> a -> m r) -> (String -> m r) }
---newtype RContT r m a = ContT { runRContT :: (String -> m r) -> (String -> a -> m r) }
-
-type ContT r a  = r -> (String -> a -> r) -> (String -> r)
-type RContT r a = r -> (String -> r) -> (String -> a -> r)
+type ContT r a  = ((a -> r) -> String -> a -> r) -> (r -> String -> r)
+type RContT r a = (r -> String -> r) -> ((a -> r) -> String -> a -> r)
 
 type PP a = forall r r'. Bi (ContT r a) (RContT r' a)
 
-type ContT0 r  = r -> (String -> r) -> (String -> r)
+type ContT0 r  = (r -> String -> r) -> (r -> String -> r)
 type RContT0 r = ContT0 r
 
 type PP0 = forall r r'. Bi (ContT0 r) (RContT0 r')
 
--- instance Monad (ContT r m) where
---   return x = ContT $ \k s -> k s x
---   m >>= f = ContT $ \k s -> runContT m (\s' x -> runContT (f x) k s') s
-
 infixl 3 <|>
 
 (<|>) :: PP a -> PP a -> PP a
-Bi f f' <|> Bi g g' = Bi (\k k' s -> f (g k k' s) k' s)
-                         (\k k' s x -> f' (g' k k' s x) k' s x)
+Bi f f' <|> Bi g (g' :: RContT r a) =
+  Bi (\k k' s -> f k (g k k' s) s)
+     (\k k' s x -> f' k (g' (k :: r -> String -> r) (k' :: a -> r) s) s x)
 
 empty :: PP0
-empty = Bi (\k k' s -> k) (\k k' s -> k)
+empty = Bi (\k k' s -> k') (\k k' s -> k')
 
 get :: ContT r String
-get = \k k' s -> k' s s
+get = \k k' s -> k (const k') s s
 
 set :: String -> ContT r ()
-set s' = \k k' s -> k' s' ()
+set s' = \k k' s -> k (const k') s' ()
 
 write :: (a -> String) -> RContT r a
-write f = \k k' s x -> k' (s ++ f x)
+write f = \k k' s x -> k (k' x) (s ++ f x)
 
 write0 :: String -> RContT0 r
-write0 x = \k k' s -> write id k k' s x
+write0 x = \k k' s -> write id k (const k') s x
 
-bicons = Bi (\k k' s xs x -> k' s (x:xs)) (\k k' s xs -> case xs of
-                                              x:xs -> k' s xs x
-                                              _ -> k)
+bicons :: Bi ((([a] -> r) -> String -> [a] -> r) -> ([a] -> a -> r) -> String -> [a] -> a -> r)
+             ((([a] -> a -> r') -> String -> [a] -> a -> r') -> ([a] -> r') -> String -> [a] -> r')
+bicons = Bi (\k k' s xs x -> k (const (k' xs x)) s (x:xs)) (\k k' s xs -> case xs of
+                                              x:xs -> k (\_ _ -> k' xs) s xs x
+                                              _ -> k' xs)
 
-binil = Bi (\k k' s -> k' s []) (\k k' s xs -> case xs of
-                                                [] -> k' s
-                                                _ -> k)
+binil :: PP [a]
+binil = Bi (\k k' s -> k (const k') s []) (\k k' s xs -> case xs of
+                                                [] -> k (k' xs) s
+                                                _ -> k' xs)
 
 many :: PP a -> PP [a]
 --many b = ((b <> many b) --> bicons) <|> binil
 many b =
-  ((Bi (\k -> first b k . first (many b) (const k))
-       (\k -> second (many b) (const k) . second b k)) --> bicons) <|> binil
+  ((Bi (first b . first (many b))
+       (second (many b) . second b)) --> bicons) <|> binil
 
 lit :: String -> PP0
-lit x = Bi (\k k' -> maybe k k' . stripPrefix x) (write0 x)
+lit x = Bi (\k k' -> maybe k' (k k') . stripPrefix x) (write0 x)
 
 char :: PP Char
-char = Bi (\k k' s -> if null s then k else k' (tail s) (head s))
+char = Bi (\k k' s -> if null s then k' else k (const k') (tail s) (head s))
           (write (:[]))
 
 int :: PP Int
