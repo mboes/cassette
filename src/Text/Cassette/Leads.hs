9663ca3f4a14b3ee0aa44572e83b35b64c7ba9ad
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-03-17 23:54:41 -0400

    Introduce 'BinL' and 'UnL' type synonyms for leads.

--- src/Text/Cassette/Leads.hs
+++ src/Text/Cassette/Leads.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE RankNTypes #-}
 module Text.Cassette.Leads where
 
 import Text.Cassette.Prim
 
 
-consL :: K7 (C ([a] -> r)) (C ([a] -> a -> r))
-            (C ([a] -> r')) (C ([a] -> a -> r'))
+-- | The type of binary leads, parameterized by the type of the left operand,
+-- the right operand, and the type of the result.
+type BinL a b c =
+  forall r r'. K7 (C (c -> r))  (C (b -> a -> r))
+                  (C (c -> r')) (C (b -> a -> r'))
+
+consL :: BinL a [a] [a]
 consL = K7 (\k k' s xs' x -> k (\s _ -> k' s xs' x) s (x:xs'))
            (\k k' s xs -> case xs of
                x:xs' -> k (\s _ _ -> k' s xs) s xs' x
@@ -13,8 +19,13 @@ consL = K7 (\k k' s xs' x -> k (\s _ -> k' s xs' x) s (x:xs'))
 nilL :: PP [a]
 nilL = shift [] nothing
 
-justL :: K7 (C (Maybe a -> r)) (C (a -> r))
-            (C (Maybe a -> r')) (C (a -> r'))
+-- | The type of binary leads, parameterized by the type of the left operand,
+-- the right operand, and the type of the result.
+type UnL a b =
+  forall r r'. K7 (C (b -> r))  (C (a -> r))
+                  (C (b -> r')) (C (a -> r'))
+
+justL :: UnL a (Maybe a)
 justL = K7 (\k k' s x -> k (\s _ -> k' s x) s (Just x))
            (\k k' s mb -> maybe (k' s mb) (k (\s _ -> k' s mb) s) mb)
 
