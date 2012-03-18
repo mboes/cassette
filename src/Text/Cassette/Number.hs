e9d4905483ef096e3563d3526c6764e04952fc81
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-03-18 15:50:45 -0400

    Cassette for integer literals.

---
+++ src/Text/Cassette/Number.hs
@@ -0,0 +1,14 @@
+-- | This module exports combinators for parsing number literals.
+module Text.Cassette.Number where
+
+import Text.Cassette.Prim
+import Text.Cassette.Leads
+import Text.Cassette.Combinator
+import Text.Cassette.Char
+
+
+-- | An integer literal, positive or negative.
+int :: PP Int
+int = intL --> many1 digit <|>
+      intL --> consL --> satisfy (== '-') <> many1 digit
+  where intL = liftL $ Sym $ K7 read show
