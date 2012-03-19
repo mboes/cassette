667dc3dc2a707428ca9dc1e69de6bc897d7905b5
Mathieu Boespflug <mboes@cs.mcgill.ca>
2012-03-19 11:16:51 -0400

    Text.Cassette module that reexports everything.

---
+++ src/Text/Cassette.hs
@@ -0,0 +1,12 @@
+module Text.Cassette
+       ( module Text.Cassette.Prim
+       , module Text.Cassette.Lead
+       , module Text.Cassette.Combinator
+       , module Text.Cassette.Char
+       , module Text.Cassette.Number ) where
+
+import Text.Cassette.Prim
+import Text.Cassette.Lead
+import Text.Cassette.Combinator
+import Text.Cassette.Char
+import Text.Cassette.Number
