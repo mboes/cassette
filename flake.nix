bc8f819152cd3cda9e4559e3676f53d725888f9f
Mathieu Boespflug <m@boespfl.ug>
2025-04-05 10:13:45 +0300

    Use nixfmt for formatting

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 {
   inputs.nixpkgs.url = "nixpkgs";
   inputs.flake-utils.url = "flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells.default =
-        let pkgs = import nixpkgs { inherit system; }; in
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
         pkgs.mkShell {
-          packages = with pkgs; [ cabal-install haskell.compiler.ghc9101 ];
-      };
+          packages = with pkgs; [
+            cabal-install
+            haskell.compiler.ghc9101
+          ];
+        };
+      formatter =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-rfc-style;
     });
 }
