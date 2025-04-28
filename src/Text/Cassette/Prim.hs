ffb9817ae92b6a108f5751c4dbaa8782e2ef3e89
Mathieu Boespflug <m@boespfl.ug>
2025-04-28 13:36:16 +0300

    Add reset1, plug1 and define replace in terms of plug1

--- src/Text/Cassette/Prim.hs
+++ src/Text/Cassette/Prim.hs
@@ -59,21 +59,29 @@ flip (K7 f g) = K7 g f
 --- strings.
 type C r = (String -> r) -> String -> r
 
--- | @'Tr' r r'@ is the type of string transformers that change answer type
--- from @r@ to @r'@ through control effects.
+-- | @'Tr' r r'@ is the type of string transformers with answer type
+-- modification from @r@ to @r'@ through control effects.
 newtype Tr r r' = Tr { unTr :: C r -> C r' }
 
 instance Category Tr where
   id = Tr id
   Tr f . Tr g = Tr (f . g)
 
--- | Replace the success continuation.
-replace :: C r -> Tr r r
-replace f = Tr (\_ k' s -> (f k' s))
-
--- | Capture continuation up to closest reset.
+-- | Capture continuation up to the closest 'reset1'.
 shift1 :: (C r -> Tr w r') -> Tr r r'
 shift1 f = Tr (\k -> unTr (f k) id)
+
+-- | Delimit what is captured by 'shift1'.
+reset1 :: Tr r r' -> C r'
+reset1 (Tr f) = f id
+
+-- | Inverse of 'shift1'.
+plug1 :: C r -> Tr r r' -> Tr w r'
+plug1 k (Tr f) = Tr (\_ -> f k)
+
+-- | Replace the success continuation.
+replace :: C r -> Tr w r
+replace k = plug1 k id
 
 push :: a -> Tr (a -> r) r
 push x = shift1 (\k -> replace (\k' s -> k (\s _ -> k' s) s x))
